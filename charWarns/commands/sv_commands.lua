0e6c7db728340e94ab7e481de90f0428cce41ec7
Boz <40073828+B0zy@users.noreply.github.com>
2023-12-27 18:52:57 -0500

    Update sv_commands.lua

--- charWarns/commands/sv_commands.lua
+++ charWarns/commands/sv_commands.lua
@@ -4,36 +4,37 @@ lia.command.add(
     {
         adminOnly = true,
         privilege = "Management - Warn Players",
-        syntax = "<string name> <string text>",
+        syntax = "<string name> <string reason>",
         onRun = function(client, arguments)
             local target = lia.command.findPlayer(client, arguments[1])
             local tChar = target:getChar()
             local oldValue = tChar:getData("warn", 0)
             tChar:setData("warn", oldValue + 1)
             local newValue = tChar:getData("warn", 1)
-            local ply = tChar:getPlayer()
-            local reason = arguments[2] or ""
-            if string.len(reason) < 1 then reason = "Not specified." end
+            local reason = table.concat(arguments, " ", 2) or "" -- Concatenate arguments starting from index 2
+            
+            if string.len(reason) < 1 then
+                reason = "Not specified."
+            end
             if newValue >= lia.config.WarnsTillBan then 
                 tChar:ban(lia.config.BanTime)
                 tChar:setData("warn", oldValue)
                 target:notify("Your character has been blocked for exceeding the allowed number of warnings!")
-               -- client:notify("The character " .. client:GetName() .. " has been blocked for exceeding the allowed number of warnings")
             end
 
-            ply:ChatPrint("You received a warning from " .. client:GetName() .. " Reason: " .. reason .. ".")
-            ply:notify("Total warnings: " .. newValue)
-            ply:ChatPrint("You were frozen for " .. lia.config.FreezeTime .. " seconds. Please read the warning!")
+            target:ChatPrint("You received a warning from " .. client:GetName() .. " Reason: " .. reason .. ".")
+            target:notify("Total warnings: " .. newValue)
+            target:ChatPrint("You were frozen for " .. lia.config.FreezeTime .. " seconds. Please read the warning!")
             client:notify("Success!")
-            ply:GodEnable()
-            ply:Freeze(true)
-            IsValid(ply)
+            target:GodEnable()
+            target:Freeze(true)
+            IsValid(target)
             timer.Simple(
                 lia.config.FreezeTime,
                 function()
-                    if IsValid(ply) then
-                        ply:GodDisable()
-                        ply:Freeze(false)
+                    if IsValid(target) then
+                        target:GodDisable()
+                        target:Freeze(false)
                     end
                 end
             )
